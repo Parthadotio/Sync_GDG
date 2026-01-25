7e086c24cf50c63d97a55153fdad33f581bf6fd3
Dx4r <100469018+Partha112@users.noreply.github.com>
2026-01-26 02:29:36 +0530

    Fixing errors

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -3,7 +3,6 @@ import Login from '../screens/Login'
 import Register from '../screens/Register'
 import Project from '../screens/Project.jsx'
 import Projectdetails from '../screens/Projectdetails.jsx'
-import About from '../screens/about'
 import UserAuth from '../auth/UserAuth'
 
 const AppRoutes = () => {
@@ -11,9 +10,6 @@ const AppRoutes = () => {
     <BrowserRouter>
          <Routes>
             <Route path='/' element= { <Login/>} />
-            <Route path='/about' element= { <About/> } />
-            <Route path='/service' element= { <div>Service</div> } />
-            <Route path='/contact' element= { <div>Contact</div> } />
             <Route path='/register' element= { <Register/> } />
             <Route path='/projects' element= { <UserAuth> <Project/> </UserAuth>} />
             <Route path='/projects/:projectId' element= { <Projectdetails/> } />
